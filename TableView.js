d3fe8205aacc412c4f6bbf681617d0b11b5f9c1e
Benjamin Albert <benjamin_albert@me.com>
2015-07-03 17:06:04 +0300

    Returnd TableView to Backbone.View

--- TableView.js
+++ TableView.js
@@ -1,18 +1,12 @@
 var TableView = Marionette.LayoutView.extend({
 	tagName: "table",
-	
+
 	render: function() {
-		//Marionette.ItemView.prototype.render.apply(this, arguments);
-		
-		this.columns = new ColumnCollectionView({
-			collection: this.model.getColumns()
-		});
+		this.columns = new ColumnCollectionView({ collection: this.model.getColumns() });
 		
 		this.$el.append( this.columns.render().el );
 		
-		this.rows = new RowCollectionView({
-			collection: this.model.getRows()
-		});
+		this.rows = new RowCollectionView({ collection: this.model.getRows() });
 		
 		this.$el.append( this.rows.render().el );
 		
